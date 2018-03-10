8faf0aa17b5dcc9db434501dcc462844dafb7ad0
Daniel Tea <daniel.tea@onehopewine.com>
2018-03-10 00:14:27 -0800

    add styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: cadetblue;
+`;
 
 class App extends React.Component {
   render () {
-    return <div>hi</div>;
+    return <Title>This is a styled component!</Title>;
   }
 };
 
