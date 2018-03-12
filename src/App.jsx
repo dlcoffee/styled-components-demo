4113adfddf730c319a9cba9e1053273350b473b0
Daniel Tea <daniel.tea@onehopewine.com>
2018-03-12 13:50:37 -0700

    add reset css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { injectGlobal } from 'styled-components'
+
+import reset from './css/reset'
+
+const baseStyles = () => {
+  injectGlobal`${reset}`;
+};
+
+const Container = styled.div`
+  background: cadetblue;
+`;
 
 const Title = styled.h1`
+  color: peachpuff;
   font-size: 1.5em;
   text-align: center;
-  color: cadetblue;
 `;
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    baseStyles();
+  };
+
   render () {
-    return <Title>This is a styled component!</Title>;
+    return <Container className="still available">
+      <Title>This is a styled component!</Title>
+    </Container>;
   }
 };
 
